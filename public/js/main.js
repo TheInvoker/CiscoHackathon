b7988f1758a85aa321cdf089ec96e702a73158a8
TheInvoker <ryans.dsouza@mail.utoronto.ca>
2016-05-15 02:42:13 -0400

    socket chat

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,45 @@
+var userObj = {
+	"Ryan.dsouza@Hotmail.ca" : {
+		'icon_url' : '',
+		'DisplayName' : 'RyanD'
+	},
+	"jasoncdu.92@gmail.com" : {
+		'icon_url' : '',
+		'DisplayName' : 'Help Bot'
+	}
+};
+
 $(document).ready(function() {
-	alert(1);
+	
+	var socket = io();
+	
+	socket.on('getChatMessagesSuccess', function(data){
+		var box = $("#chatbox").empty();
+		var items = data.items;
+		for(var i=items.length-1; i>=0; i-=1) {
+			
+			var container = $("<div class='commentBox'/>");
+			container.append("<div>" + userObj[items[i].personEmail].DisplayName + "</div>");
+			container.append("<div>" + items[i].text + "</div>");
+			box.append(container);
+		}
+	});
+	
+	socket.on('setChatMessagesSuccess', function(data){
+		
+	});
+	
+	socket.emit('getChatMessages');
+	setInterval(function() {
+		socket.emit('getChatMessages');
+	}, 2000);
+	
+	
+	$("#submit").click(function() {
+		var msg = $("#msgField").val();
+		$("#msgField").val("");
+		socket.emit('setChatMessages', {
+			'message' : msg
+		});
+	});
 });
