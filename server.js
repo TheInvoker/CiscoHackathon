ab4e032d4e6c253ca7cbda62f2871912c1bbc62a
TheInvoker <ryans.dsouza@mail.utoronto.ca>
2016-05-15 00:10:09 -0400

    zues stuff

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var https = require('https');
+var http = require('http');
 var fs = require('fs');
+var qs = require("querystring");
+var request = require('request');
 
+var ZUES_TOKEN = "8fea82fd";
 var SPARK_PERSONAL_ACCESS_TOKEN = 'YjM2ZmYwY2QtYzBmZS00ZGUxLTlkMWItODc3MzliODI3NmE1OGNjYjA1OGQtYTBj';
 
 function writeCiscoData(chunk, cb) {
@@ -15,7 +19,50 @@ function writeJSON(file, json, cb) {
 	}); 
 }
 
-
+function readLogFromZues(cb) {
+	var post_req = http.request({
+		host: 'api.site1.ciscozeus.io',
+		path: '/logs/' + ZUES_TOKEN + '/',
+		//port: 80,
+		method: 'GET',
+		headers: {
+			'Content-type' : 'application/x-www-form-urlencoded'
+		}
+	}, function(res) {
+		console.log(1);
+		res.on('data', function (chunk) {
+			console.log(2);
+			cb(chunk);
+		});
+	});
+	post_req.write(JSON.stringify({
+		"attribute_name" : 'TEST'
+	}));
+	post_req.on('error', function(e) {
+		console.log(3);
+		console.error(e);
+	});
+	post_req.end();
+}
+function sendLogToZues(logName, cb) {
+	var options = { 
+		method: 'POST',
+		url: 'http://api.site1.ciscozeus.io/logs/'+ZUES_TOKEN+'/'+logName+'/',
+		headers: { 
+			'content-type': 'application/x-www-form-urlencoded',
+			'cache-control': 'no-cache' 
+		},
+		form: { 
+			logs: '[{"test":"value1"}]' 
+		} 
+	};
+	request(options, function (error, response, body) {
+		if (error) {
+			throw new Error(error);
+		}
+		cb(body);
+	});
+}
 function getRoom(titleFilter, typeFilter, cb) {
 	var post_req = https.request({
 		host: 'api.ciscospark.com',
@@ -40,7 +87,6 @@ function getRoom(titleFilter, typeFilter, cb) {
 	});
 	post_req.end();
 }
-
 function writeMessage(item, msg, cb) {
 	var post_req = https.request({
 		host: 'api.ciscospark.com',
@@ -69,11 +115,24 @@ function writeMessage(item, msg, cb) {
 
 
 
-
+/*
 getRoom("Jason", "direct", function(item) {
 	writeMessage(item, "XYZ", function(data) {
 	});
 });
+*/
 
+/*
+readLogFromZues(function(data) {
+	console.log(data);
+});
+*/
+
+
+
+
+sendLogToZues("TEST123", function(data) {
+	console.log(data);
+});
 
 
